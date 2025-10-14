92155c5dbaf4be05b5c7ccf8383d039e90dff794
moyaojun <41281346+Mask-MJ@users.noreply.github.com>
2025-10-14 16:26:45 +0800

    feat: init

--- packages/types/src/basic.d.ts
+++ packages/types/src/basic.d.ts
@@ -5,42 +5,6 @@ interface BasicOption {
   value: string;
 }
 
-/** 用户信息 */
-interface UserInfo {
-  /**
-   * 头像
-   */
-  avatar: string;
-  /**
-   * 用户昵称
-   */
-  realName: string;
-  /**
-   * 用户角色
-   */
-  roles?: string[];
-  /**
-   * 用户id
-   */
-  userId: string;
-  /**
-   * 用户名
-   */
-  username: string;
-  /**
-   * 用户描述
-   */
-  desc: string;
-  /**
-   * 首页地址
-   */
-  homePath: string;
-  /**
-   * accessToken
-   */
-  token: string;
-}
-
 interface TabDefinition extends RouteLocationNormalized {
   /**
    * 标签页的key
@@ -50,4 +14,4 @@ interface TabDefinition extends RouteLocationNormalized {
 
 type ClassType = Array<object | string> | object | string;
 
-export type { BasicOption, ClassType, TabDefinition, UserInfo };
+export type { BasicOption, ClassType, TabDefinition };
